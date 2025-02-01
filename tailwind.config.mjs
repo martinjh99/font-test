864e91cbce1f19b7bf0668bcf81a97f42ff7e7d7
Martin Hooper <martin@mjhooper.co.uk>
2025-02-01 07:47:23 +0000

    updated to latest astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,32 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		colors: {
-			red: {
-				100: "#ff0000",
-				200: "#110000",
-			},
-			green: {
-				100: "#002200",
-				200: "#007700",
-				300: "#00ff00",
-				400: "#11a500",
-			},
-		},
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        colors: {
+            red: {
+                100: "#ff0000",
+                200: "#440000",
+            },
+            green: {
+                100: "#002200",
+                200: "#007700",
+                300: "#00ff00",
+                400: "#11a500",
+            },
+            black: "#00000",
+            white: "#ffffff",
+        },
 
-	},
-	fontFamily: {
-		teko: ["Teko", "serif"]
-	},
+        // maybe move these into extend object if you need to keep defaults
+        fontFamily: {
+            teko: ["Teko", "serif"],
+            mont: ["Montserrat","serif"]
+        },
+    
+        fontSize: {
+            small: "10px",
+            med: "15px",
+            lrg: "30px",
+            xxxl: "50px"
+        },
 
-	fontSize: {
-		small: "10px",
-		med: "15px",
-		lrg: "30px",
-	},
+        extend: {},
+    },
+
+    plugins: [],
+
 }
-
-	
-	extend: {}
-	plugins: []
